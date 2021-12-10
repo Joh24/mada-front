791ea8da11c7889fcadcf63249eb26e8d42dfdd5
joh501 <rasamimanana.johny@orange.mg>
2021-12-10 14:24:40 +0300

    login and register template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
+import { MessageComponent } from './message/message.component';
+import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import { ProfileListComponent } from './profile-list/profile-list.component';
+import { MessageListComponent } from './message-list/message-list.component';
+import { RegisterComponent } from './register/register.component';
+import { SittingComponent } from './sitting/sitting.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'list', component : ProfileListComponent },
+  {path : 'profile', component : ProfileComponent},
+  {path : '', component : LoginComponent},
+  {path : 'message', component : MessageComponent},
+  {path : 'register', component : RegisterComponent},
+  {path : 'sitting', component : SittingComponent},
+  {path : 'messages', component : MessageListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileListComponent } from './profile-list/profile-list.component';
+import { ProfileComponent } from './profile/profile.component';
+import { MessageListComponent } from './message-list/message-list.component';
+import { MessageComponent } from './message/message.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { SittingComponent } from './sitting/sitting.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileListComponent,
+    ProfileComponent,
+    MessageListComponent,
+    MessageComponent,
+    LoginComponent,
+    RegisterComponent,
+    SittingComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
